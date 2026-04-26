7b6bb96d16a8c9539bd84cd1b8ed1d6c257050e8
Gurka Celk <gurkancelk00@gmail.com>
2026-04-27 01:18:36 +0300

    Update page.tsx to use TempoLandingPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { TempoGameApp } from "./components/tempo-game-app";
-import { TempoGameStateProvider } from "./lib/tempo-game-state";
+import { TempoLandingPage } from "./components/tempo-landing-page";
 
 export default function Home() {
-  return (
-    <TempoGameStateProvider>
-      <TempoGameApp />
-    </TempoGameStateProvider>
-  );
+  return <TempoLandingPage />;
 }
